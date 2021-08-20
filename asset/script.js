bb0110d525eb4666cf5ce520b3fe42a31653bc69
Jeffery <jefferywojo1998@gmail.com>
2021-08-19 22:15:13 -0500

    finish up the night

--- asset/script.js
+++ asset/script.js
@@ -5,28 +5,35 @@ const option1 = $("#o1")
 const option2 = $("#o2")
 const option3 = $("#o3")
 const option4 = $("#o4")
+const main = $('#main')
 let index = 0
+let allScores = []
+
 
 let question =[{
     text:"what is the best number",
     options:["1","2","3","4"],
-    Correct:"1"
+    Ccrrect:"1"
 }, {
     text:"what is not the best number",
     options:["1","2","3","4"],
-    Correct:"4"
+    correct:"4"
 }]
+
 
 function startGame(){
     console.log("start")
-    $('#supWrap').remove();
+    $('#title').remove();
+    $('#start').remove();
+    $("main").show();
     loadNextQuestion()
     
 }
 
 function loadNextQuestion() {
     questionTitle.text(question[index].text);
-    
+    i
+    index++;
 }
 
 startBtn.on('click', startGame)
