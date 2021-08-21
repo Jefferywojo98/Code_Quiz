20f3475a05507c42df37e8f7552979956977f5a6
Jeffery <jefferywojo1998@gmail.com>
2021-08-21 01:24:27 -0500

    trying a different approch and doing the time first then the score and then the quiz

---
+++ script.js
@@ -0,0 +1,13 @@
+var question = [{
+    title: "what is my favorite number?",
+    options: ["1", "2", "3", "4"],
+    answer: "3"
+},{
+    title: "what is my favorite number?",
+    options: ["1", "2", "3", "4"],
+    answer: "3"
+}]
+var nextQuestion = 0
+var score = 0
+var timeRemaining = 0
+var timer
